941168a42dd6046473672d94595c3bd97e28d6ee
Lara Isak <luckasti.larry@gmail.com>
2021-06-30 14:54:32 +0200

    ✨ fetch posts from api and display them individually

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('Hello 8-bit world!');
+const blogPost = document.querySelector('.blogPost');
+const url = 'https://jsonplaceholder.typicode.com/posts';
+let posts = [];
+
+async function getBlogPost() {
+  const response = await fetch(url);
+  const blogs = await response.json();
+
+  console.log(blogs);
+
+  blogs.forEach((blog) => {
+    let post = document.createElement('div');
+    let postTitle = document.createElement('h1');
+    let postText = document.createElement('p');
+    let postMaker = document.createElement('p');
+
+    postTitle.innerHTML = blog.title;
+    postText.innerHTML = blog.body;
+    postMaker.innerHTML = `By: ${blog.userId}`;
+
+    post.appendChild(postTitle);
+    post.appendChild(postText);
+    post.appendChild(postMaker);
+    blogPost.appendChild(post);
+  });
+
+  console.log(blogPost);
+};
+
+
+getBlogPost();
+
