88b40550fd80ef24c6acd780e13a104349655fba
Lara Isak <luckasti.larry@gmail.com>
2021-07-03 18:53:56 +0200

    ♻️ change no. of initial / on button click posts

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const blogPost = document.querySelector('.blogPost');
 const url = 'https://jsonplaceholder.typicode.com/posts?userId=1&userId=2&userId=3';
 let posts = [];
-let currentPosts = 10;
+let currentPosts = 6;
 const loadBtn = document.querySelector('.loadMore');
 
 async function getBlogPost() {
@@ -33,14 +33,15 @@ getBlogPost();
 
 
 loadBtn.addEventListener('click', () => {
-  const postList = [...document.querySelectorAll('.blogPost div')];
+  let displayedPosts = document.querySelectorAll('.blogPost div');
+  const postList = [...displayedPosts];
 
-  for(let i = currentPosts; i < currentPosts + 10; i++) {
+  for(let i = currentPosts; i < currentPosts + 6; i++) {
     if(postList[i]) {
       postList[i].style.display = 'flex';
     }
   }
-  currentPosts += 10;
+  currentPosts += 6;
 
   if(currentPosts >= postList.length) {
     loadBtn.style.display = 'none';
