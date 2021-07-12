2d7d4323b96f16ad2d971ed7562b6e527abb0a13
Lara Isak <luckasti.larry@gmail.com>
2021-07-12 13:54:35 +0200

    🎨 replace generic div tag with an article tag

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ async function getBlogPost() {
   console.log(blogs);
 
   blogs.forEach((blog) => {
-    let post = document.createElement('div');
-    let postTitle = document.createElement('h1');
+    let post = document.createElement('article');
+    let postTitle = document.createElement('h2');
     let postText = document.createElement('p');
     let postMaker = document.createElement('p');
 
@@ -33,7 +33,7 @@ getBlogPost();
 
 
 loadBtn.addEventListener('click', () => {
-  let displayedPosts = document.querySelectorAll('.blogPost div');
+  let displayedPosts = document.querySelectorAll('.blogPost article');
   const postList = [...displayedPosts];
 
   for(let i = currentPosts; i < currentPosts + 6; i++) {
